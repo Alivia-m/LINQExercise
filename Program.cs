1b38f79d115342b6f1b587d9f56b3b6a44b7ff8e
Alivia-m <aliviagayle@icloud.com>
2021-06-30 18:36:57 -0500

    edit later

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExercise
+{ }
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> videoGames = new List<string>
+            {"Horizon Zero Dawn", "Forza", "Infamous", "Overwatch"};
+
+        foreach (string game in SortByLength(videoGames))
+        {
+            Console.WriteLine(game);
+        }
+        static IEnumerable<string>
+            SortByLength(IEnumerable<string> e)
+            {
+            var sorted = from game in e
+                         orderby game.Length ascending
+                         select game;
+            return sorted;
+        }
+    } 
+}
